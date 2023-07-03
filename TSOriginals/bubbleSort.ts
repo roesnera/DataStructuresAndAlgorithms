53e6674839ca9d1e295df023fface95fde7c3a66
= <adam.roesner@generation.org>
2023-07-03 12:49:58 -0400

    adds my first attempt at a bubble sort algorithm in like a decade lol

---
+++ TSOriginals/bubbleSort.ts
@@ -0,0 +1,39 @@
+//bubble sort
+// requires a nested loop
+// the outer loop will iterate once for each element in the array
+// the inner loop will iterate once for each element in the array minus the number of completed outer loop iterations
+// inside the inner loop, if the early element is larger than the later element, swap the two
+
+/* 
+    function bubbleSort(array, comparisonFunction, swapFunction)
+    array: array to be sorted
+    comparisonFunction: function that will be used to make the comparison
+    swapFunction: function that will be used to swap two elements in an array at the given indexes
+*/
+function bubbleSort<T>(
+  arr: T[],
+  compare: (a: T, b: T) => number,
+  swap: (arr: T[], ind1: number, ind2: number) => void
+): T[] {
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - 1 - i; j++) {
+      const comparison = compare(arr[j], arr[j + 1]);
+      if (comparison > 0) {
+        swap(arr, j, j + 1);
+      }
+    }
+  }
+  return arr;
+}
+
+function compare(a: number, b: number): number {
+  return a - b;
+}
+
+const swap = (arr: number[], ind1: number, ind2: number) => {
+  [arr[ind1], arr[ind2]] = [arr[ind2], arr[ind1]];
+};
+
+let arr = [1, 2, 0, 9, 7, 5, 6];
+
+console.log(bubbleSort(arr, compare, swap));
---
+++ jsOut/bubbleSort.js
@@ -0,0 +1,31 @@
+"use strict";
+//bubble sort
+// requires a nested loop
+// the outer loop will iterate once for each element in the array
+// the inner loop will iterate once for each element in the array minus the number of completed outer loop iterations
+// inside the inner loop, if the early element is larger than the later element, swap the two
+/*
+    function bubbleSort(array, comparisonFunction, swapFunction)
+    array: array to be sorted
+    comparisonFunction: function that will be used to make the comparison
+    swapFunction: function that will be used to swap two elements in an array at the given indexes
+*/
+function bubbleSort(arr, compare, swap) {
+    for (let i = 0; i < arr.length - 1; i++) {
+        for (let j = 0; j < arr.length - 1 - i; j++) {
+            const comparison = compare(arr[j], arr[j + 1]);
+            if (comparison > 0) {
+                swap(arr, j, j + 1);
+            }
+        }
+    }
+    return arr;
+}
+function compare(a, b) {
+    return a - b;
+}
+const swap = (arr, ind1, ind2) => {
+    [arr[ind1], arr[ind2]] = [arr[ind2], arr[ind1]];
+};
+let arr = [1, 2, 0, 9, 7, 5, 6];
+console.log(bubbleSort(arr, compare, swap));
