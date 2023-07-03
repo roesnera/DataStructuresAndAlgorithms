861be3e2cd3db0dba61442fa3f5d3a0cf57fb40e
= <adam.roesner@generation.org>
2023-07-03 16:07:47 -0400

    fills in some notes on insertion sort

--- TSOriginals/insertionSort.ts
+++ TSOriginals/insertionSort.ts
@@ -2,8 +2,14 @@
 
 
 function insertionSort<T>(arr: T[]): T[] {
+    // iterating over every element of the array
     for( let i = 1; i < arr.length; i++){
+        // grabbing the current value for comparisons
         let current = arr[i];
+        // this loop works backwards from the element immediately to the left of i
+        // it will continue iterating until the value at j is no longer greater than the value at i
+        // each iteration, it swaps the element at j one to the right, 
+        // and the element at j+1 (which should always be the element at i) to the left
         for( let j = i - 1; j >= 0 && arr[j] > current; j--){
             [arr[j], arr[j+1]] = [current, arr[j]];
         }
